fa60d91b55a794546aa4847f0883f967aca4fb3a
Huong Thien Do <dothienhuong2701@gmail.com>
2024-03-24 04:57:44 -0500

    display the bar chart for each city

--- app/src/chart-and-chatbot/ChartData.js
+++ app/src/chart-and-chatbot/ChartData.js
@@ -4,28 +4,21 @@ import './ChartData.css'
 import BarChartUI from './chart-switching/BarChartUI';
 import LineChartUI from './chart-switching/LineChartUI';
 
-const ChartData = () => {
-  const chartRef = useRef(null);
-  const chartInstanceRef = useRef(null);
-  const [tab, setTab] = useState('bar')
+const ChartData = ({ dataSet }) => {
+  const [tab, setTab] = useState('bar');
 
   const changeTab = () => {
-    if (tab === 'bar') {
-      setTab('line')
-    } else {
-      setTab('bar')
-    }
-  }
-
+    setTab(tab === 'bar' ? 'line' : 'bar');
+  };
 
   return (
     <div>
-      {tab === 'bar' ? <BarChartUI /> : <LineChartUI />}
-      <button className='swicthing-button' onClick={changeTab}>
+      {tab === 'bar' ? <BarChartUI dataSet={dataSet} /> : <LineChartUI dataSet={dataSet} />}
+      <button className='switching-button' onClick={changeTab}>
         {tab === 'bar' ? 'Switch to Linear Chart' : 'Switch to Bar Chart'}
       </button>
     </div>
   );
-}
+};
 
 export default ChartData;
