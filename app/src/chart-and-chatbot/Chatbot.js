35d8cd867cc4c086e9c4fbfa06f5f2c5fc65b0b5
aldoprogrammer <aldobesma@gmail.com>
2024-03-24 14:10:39 +0700

    fix chatbox

--- app/src/chart-and-chatbot/Chatbot.js
+++ app/src/chart-and-chatbot/Chatbot.js
@@ -25,14 +25,35 @@ const steps = [
   {
     id: '5',
     options: [
-      { value: '1', label: 'Yes', trigger: '6' },
-      { value: '2', label: 'No', trigger: '7' },
+      { value: '1', label: 'Want to know about crime rate?', trigger: '6' },
+      { value: '2', label: 'Any malls nearby this', trigger: '7' },
+      { value: '3', label: 'Schools nearby this', trigger: '2' },
+      { value: '4', label: 'How far is this (city1) from (city2)?', trigger: '2' },
     ],
   },
   {
     id: '6',
-    message: 'Great, let me know what you need and I will do my best to help.',
-    trigger: '8',
+    options: [
+      { value: '1', label: 'Jakarta', trigger: '10' },
+      { value: '2', label: 'New York', trigger: '7' },
+    ],
+  },
+  {
+    id: '10',
+    message: 'The crime rate at Jakarta is 60% last year',
+    trigger: '9',
+  },
+  {
+    id: '9',
+    message: 'Do you want to ask again?',
+    trigger: '11',
+  },
+  {
+    id: '11',
+    options: [
+      { value: '1', label: 'Yes', trigger: '5' },
+      { value: '2', label: 'No', trigger: '8' },
+    ],
   },
   {
     id: '7',
